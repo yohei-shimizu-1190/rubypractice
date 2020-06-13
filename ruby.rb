a2bbf5e8771689f0363fb0cbcae1dc1d53470327
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-06-13 21:59:37 +0900

    [paiza]  * を入力値分だけ出力

--- ruby.rb
+++ ruby.rb
@@ -1,17 +1,2 @@
-def near_ten(num)
-  quotient = num % 10
-  p quotient
-  # if quotient  <= 2 || quotient >= 8
-  #   puts "True"
-  # else
-  #   puts "False"
-  # end
-end
-near_ten(12)
-# →True
-near_ten(17)
-# →False
-near_ten(129)
-# →True
-
-
+num = gets.to_i
+puts "*" * num
