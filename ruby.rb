5d44a2d2f9f603e6fed6ff1b1f898d9b9259757d
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-06-15 12:07:17 +0900

    [dorill] 速度違反チェック

--- ruby.rb
+++ ruby.rb
@@ -1,14 +1,35 @@
-def in1to10(num,outside_mode)
-  if (num >= 1 && num <= 10) || outside_mode
-    puts "True"
+def caught_speeding(speed, is_birthday)
+  if is_birthday
+    your_speed = speed / 5
   else
-    puts "False"
+    your_speed = speed
+  end
+  if your_speed.round >= 81
+    p "2"
+  elsif your_speed.round <= 60
+    p "0"
+  else
+    p "1"
   end
 end
 
-in1to10(5, false)
-# →True
-in1to10(11, false)
-# →False
-in1to10(11, true)
-# →True
+
+def caught_speeding(speed, is_birthday)
+  speed_rounded = speed.round(1)
+
+  if speed_rounded <= 60
+    point = 0
+  elsif speed_rounded <= 80
+    point = 1
+  else
+    point = 2
+  end
+  puts is_birthday ? point * 5 : point
+end
+
+caught_speeding(200, false)
+#  → 0
+caught_speeding(85, false)
+#  → 1
+caught_speeding(505, true)
+#  → 0
