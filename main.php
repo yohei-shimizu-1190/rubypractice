25b34d8580fde16574a6eac2fe8f6f7e8564f698
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-06-17 18:38:57 +0900

    [add] 関数の返り値の型指定

--- main.php
+++ main.php
@@ -1,15 +1,13 @@
 <?php
-// declareをつけないと、"99" という文字列の数字がphpの変換によって通ってしまう。
-// declareで強い片付けを実行する
+
 declare(strict_types=1);
 
-// 受け取る引数の型を指定できる！ : voidでは返り値の型は自由としている
-function showInfo(string $name, int $score): void
+//  : string の前に?をつけることで、返り値が stringか null のどちらかなら通る
+// 引数の型にもつけられるが、null以外の方法はない
+function getAward(?int $score): ?string
 {
-  echo $name . ":" . $score . PHP_EOL;
+  return $score >= 80 ? "Gold Medal" : null;
 }
 
-showInfo('taguchi', "99");
-
-// Fatal error: Uncaught TypeError: Argument 2 passed to showInfo() must be of the type int, string given, called in /Users/yohei/Desktop/rubypractice/main.php on line 11 and defined in /Users/yohei/Desktop/rubypractice/main.php:6
-// 2番めの引数（argument） がintかと思っていたが、stringを受け取ったというエラーになる！
+echo getAward(100); // Gold Medal
+echo getAward(30); // null
