95d3f552866cdc90dca34e58e173db448a62d707
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-06-17 17:46:51 +0900

    [add] 関数の使い方

--- main.php
+++ main.php
@@ -1,52 +1,9 @@
 <?php
-
-// 10回繰り返す
-for ($i = 1; $i <= 10; $i++) {
-  echo "$i - Hellow" . PHP_EOL;
+// 仮引数 ="hoge" で引数がない場合の値を設定できる
+function showAd($message = 'blank')
+{
+  echo "------- $message ---------" . PHP_EOL;
 }
-
-
-// 10回繰り返すが4のときだけ飛ばす
-for ($i = 1; $i <= 10; $i++) {
-  if ($i === 4) {
-    continue;
-  }
-  echo $i . PHP_EOL;
-}
-
-// 10回繰り返すが3の倍数のときだけ飛ばす
-for ($i = 1; $i <= 10; $i++) {
-  if ($i % 3 === 0) {
-    continue;
-  }
-  echo $i . PHP_EOL;
-}
-
-// 10回繰り返すが3の倍数のときだけ飛ばす
-for ($i = 1; $i <= 10; $i++) {
-  if ($i === 4) {
-    break;
-  }
-  echo $i . PHP_EOL;
-}
-
-
-
-
-
-// HPが0まで繰り返す
-$hp = 100;
-
-while ($hp > 0) {
-  echo "YOUR HP -- $hp" . PHP_EOL;
-  $hp -= 15;
-}
-
-
-// HPが最初の条件以下でも1回は実行する（ ドゥーホワイル文 ）
-$hp = 10;
-
-do {
-  echo "YOUR HP -- $hp" . PHP_EOL;
-  $hp -= 15;
-} while ($hp > 0);
+// 実引数
+showAd('hello');
+showAd();
