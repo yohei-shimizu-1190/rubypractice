ba3ebef732f0dc5bf7b9cf09597f3cf3eb3882fb
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-06-17 12:23:09 +0900

    [dorill]  正規表現 include

--- main.rb
+++ main.rb
@@ -1,2 +1,23 @@
-p "Hello"
-p "World"
+def cat_dog(str)
+  if str.match(/cat/) && str.match(/dog/)
+    p "True"
+  else
+    p "False"
+  end
+end
+
+# 答えの通りincludeの方がよかったな
+def cat_dog(str)
+  if str.include?("cat") && str.include?("dog")
+    puts "True"
+  else
+    puts "False"
+  end
+end
+  
+cat_dog("catdog") 
+# → True
+cat_dog("catcat")
+#  → False
+cat_dog("1cat1cadodog") 
+# → True
