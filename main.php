565afcb229edd3f92e7d9a5ec352c5075bdd9e2d
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-06-17 18:09:12 +0900

    [php] 無名関数

--- main.php
+++ main.php
@@ -1,13 +1,14 @@
 <?php
 
-$rate = 1.1; // グローバルスコープ
 
 function sum($a, $b, $c)
 {
-  // global $rate;
-  // とすることでグローバルスコープが関数内でも使える
-  $rate = 1.08;  // 今回はローカルスコープでやる
   return $a + $b + $c * $rate;
 }
 
-echo sum(100, 300, 500) + sum(200, 100, 100) . "円です";
+// 無名関数
+$sum = function ($a, $b, $c) {
+  return $a + $b + $c;
+};
+
+echo $sum(100, 300, 200) . PHP_EOL;
