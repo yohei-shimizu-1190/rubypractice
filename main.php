a268d7687836ae1fd50273fc6007cbee504db8cd
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-06-17 10:37:08 +0900

    [php] while for文による繰り返し処理

--- main.php
+++ main.php
@@ -1,18 +1,52 @@
 <?php
-$signal = 'green';
 
-switch ($signal) {
-  case 'red':
-    echo "止まれ" . PHP_EOL;
-    break;
-  case 'yellow':
-    echo " 注意" . PHP_EOL;
-    break;
-  case 'blue':
-  case 'green':
-    echo "すすめるよ" . PHP_EOL;
-    break;
-  default:
-    echo '信号機の故障です' . PHP_EOL;
-    break;
+// 10回繰り返す
+for ($i = 1; $i <= 10; $i++) {
+  echo "$i - Hellow" . PHP_EOL;
 }
+
+
+// 10回繰り返すが4のときだけ飛ばす
+for ($i = 1; $i <= 10; $i++) {
+  if ($i === 4) {
+    continue;
+  }
+  echo $i . PHP_EOL;
+}
+
+// 10回繰り返すが3の倍数のときだけ飛ばす
+for ($i = 1; $i <= 10; $i++) {
+  if ($i % 3 === 0) {
+    continue;
+  }
+  echo $i . PHP_EOL;
+}
+
+// 10回繰り返すが3の倍数のときだけ飛ばす
+for ($i = 1; $i <= 10; $i++) {
+  if ($i === 4) {
+    break;
+  }
+  echo $i . PHP_EOL;
+}
+
+
+
+
+
+// HPが0まで繰り返す
+$hp = 100;
+
+while ($hp > 0) {
+  echo "YOUR HP -- $hp" . PHP_EOL;
+  $hp -= 15;
+}
+
+
+// HPが最初の条件以下でも1回は実行する（ ドゥーホワイル文 ）
+$hp = 10;
+
+do {
+  echo "YOUR HP -- $hp" . PHP_EOL;
+  $hp -= 15;
+} while ($hp > 0);
