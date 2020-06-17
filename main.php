0a99a9f0982e5c5240e90bbe310782b1d273c682
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-06-17 10:24:15 +0900

    [php] switch文 信号機

--- main.php
+++ main.php
@@ -1,8 +1,18 @@
 <?php
-echo "Hello";
-echo "World";
-// echoは文とりあえずecho派でいこうかな
+$signal = 'green';
 
-print "Hello";
-print "World";
-// printは式
+switch ($signal) {
+  case 'red':
+    echo "止まれ" . PHP_EOL;
+    break;
+  case 'yellow':
+    echo " 注意" . PHP_EOL;
+    break;
+  case 'blue':
+  case 'green':
+    echo "すすめるよ" . PHP_EOL;
+    break;
+  default:
+    echo '信号機の故障です' . PHP_EOL;
+    break;
+}
