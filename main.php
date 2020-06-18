40263af5931feedbc7ebcd6625b7c4d98b16a1d7
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-06-18 10:22:26 +0900

    [dorill] .xyzを含むとだめ xyzならおk 正規表現

--- main.php
+++ main.php
@@ -1,35 +1,8 @@
 <?php
 
-$scores = [
-  90, 40, 100
-];
-
-$scores[0] = 20;
-echo $scores[0]; //20
-
-
 // 配列の呼び出し方を変えられる（左をキーという）
 $scores = [
   'first' => 90,
   'second' => 40,
   'third' => 100
 ];
-
-echo $scores['third']; //100
-
-var_dump($scores);
-// array(3) {
-//   ["first"]=>
-//   int(90)
-//   ["second"]=>
-//   int(40)
-//   ["third"]=>
-//   i
-
-print_r($scores);
-// Array
-// (
-//     [first] => 90
-//     [second] => 40
-//     [third] => 100
-// )
--- main.rb
+++ main.rb
@@ -1,23 +1,15 @@
-def cat_dog(str)
-  if str.match(/cat/) && str.match(/dog/)
+def xyz_there(str) 
+  if str.include?('.xyz')
+    p "False"
+  elsif str.include?('xyz')
     p "True"
   else
     p "False"
   end
 end
-
-# 答えの通りincludeの方がよかったな
-def cat_dog(str)
-  if str.include?("cat") && str.include?("dog")
-    puts "True"
-  else
-    puts "False"
-  end
-end
-  
-cat_dog("catdog") 
+xyz_there('abcxyz') 
 # → True
-cat_dog("catcat")
-#  → False
-cat_dog("1cat1cadodog") 
+xyz_there('abc.xyz') 
+# → False
+xyz_there('xyz.abc') 
 # → True
