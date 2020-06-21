4cc1fc2ff7dba6123bc5e05e0aed0d439ee71747
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-06-21 15:19:45 +0900

    [drill] a,b,cを重複していなければ足す

--- main.rb
+++ main.rb
@@ -1,9 +1,20 @@
-def count_coxe(str)
-  p str.scan(/co.e/).length
+def lone_sum(a, b, c) 
+  if a == b && b == c
+    p 0
+  elsif a == b
+    p c
+  elsif a == c
+    p b
+  elsif b == c
+    p a
+  else
+    p (a + b + c)
+  end
 end
-count_coxe('aaacodebbb') 
-# → 1
-count_coxe('codexxcode') 
-# → 2
-count_coxe('cozexxcope')
+lone_sum(1, 2, 3) 
+# → 6
+lone_sum(3, 2, 3)
 #  → 2
+lone_sum(3, 3, 3) 
+# → 0
+
