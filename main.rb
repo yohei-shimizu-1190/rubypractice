450b82f4817cdd6c8b8b7b72229de7df9e0e3e6d
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-06-23 14:30:21 +0900

    [drill] 配列から偶数の個数を調べる

--- main.rb
+++ main.rb
@@ -1,9 +1,11 @@
-def count_hi(str) 
-  p str.scan('hi').length
+def count_evens(array) 
+  evenArray = array.select { |n| n % 2 == 0  }
+  p evenArray.length
 end
-count_hi('abc hi ho') 
-# → 1
-count_hi('ABChi hi') 
-# → 2
-count_hi('hihi') 
-# → 2
+
+count_evens([2, 1, 2, 3, 4])
+#  → 3
+count_evens([2, 2, 0]) 
+# → 3
+count_evens([1, 3, 5]) 
+# → 0
