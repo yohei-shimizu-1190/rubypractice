e957612c52c8aa49aee24d17a346fec2347b242d
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-06-24 22:13:59 +0900

    [paiza] ジュースの分配

--- main.rb
+++ main.rb
@@ -1,11 +1,4 @@
-def count_evens(array) 
-  evenArray = array.select { |n| n % 2 == 0  }
-  p evenArray.length
-end
+mount = gets.chomp.to_i
+cup = gets.chomp.to_i
 
-count_evens([2, 1, 2, 3, 4])
-#  → 3
-count_evens([2, 2, 0]) 
-# → 3
-count_evens([1, 3, 5]) 
-# → 0
+p mount / cup
