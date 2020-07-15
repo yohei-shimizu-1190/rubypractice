01b2ea184afede894452208669db80959f87baad
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-07-15 23:28:50 +0900

    関数宣言と関数式（無名関数）の違い

--- main.js
+++ main.js
@@ -5,9 +5,13 @@ function hello() {
 
 hello();
 
-// 無名関数
+// 関数式（無名関数）
 let hello = function () {
   console.log('hello');
 }
 
 hello();
+
+// 関数宣言と無名関数の大きな違いは、読み込まれるタイミング
+// 関数宣言では、呼び出すためのhello();を関数の前に持っていっても正常に動作する
+// これはJavaScriptでは関数宣言が先に全て読み込まれるためである
