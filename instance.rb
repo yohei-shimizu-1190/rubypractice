7081a2afb558b7acbfacc48357efe03b1cbd1022
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-07-15 15:25:56 +0900

    [add]クラス変数を使用してレビュー数をカウント

--- instance.rb
+++ instance.rb
@@ -1,4 +1,10 @@
 class Review
+  @@review_count = 0
+  # クラス変数↑（クラス全体で使用できる変数です。クラスを通して、値が共通となる情報に使用します。クラス変数の特性として、いくらインスタンスを生成しようと初期化されることがない点があります）
+  def self.get_review_count
+    return @@review_count
+  end
+  
   def initialize
     puts "タイトルを入力してください"
     @title = gets.chomp
@@ -6,7 +12,9 @@ class Review
     @genre = gets.chomp 
     puts "感想を入力してください"
     @impression = gets.chomp
+    @@review_count += 1
   end
+
 
   def show_review
     line = "----------------------"
@@ -17,4 +25,7 @@ class Review
 end
 
 review = Review.new
+# これもクラスメソッド↑
 review.show_review
+
+puts Review.get_review_count
