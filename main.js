4d4e207ec956e49a7ad0ccfcfaa3780ace030688
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-07-15 23:19:53 +0900

    [tech]returnの必要性

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-function sayHello() {
-  console.log('hello');
+function calc(num1, num2) {
+  return num1 * num2;
+  // returnで返り値を明示する必要あり
 }
 
-function sayName(name) {
-  console.log(name)
-}
-
-let myName = 'yamada';
-sayHello();
-sayName(myName);
+let num1 = 5;
+let num2 = 3;
+console.log(calc(num1, num2));
