7bdb9ed6a371a61b258c492d5a5603227e88137e
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-07-18 00:03:54 +0900

    window.alert , for文

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
-// 関数宣言
-function hello() {
-  console.log('hello');
+for (let i = 0; i < 10; i += 1) {
+  console.log('hello')
 }
 
-hello();
 
-// 関数式（無名関数）
-let hello = function () {
-  console.log('hello');
-}
-
-hello();
-
-// 関数宣言と無名関数の大きな違いは、読み込まれるタイミング。関数宣言では、呼び出すためのhello();を関数の前に持っていっても正常に動作する。これはJavaScriptでは関数宣言が先に全て読み込まれるためである
+window.alert('alert!!');
+// ページを開くとモーダルウィンドウが出る
