f425ecb233a2ef9622824ecd7090db62141a4f78
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-07-19 23:22:06 +0900

    [tech] classList.add

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-// ボタン2を押したらテキスト内容が変更する操作
-
-
-let btn2 = document.getElementById("#Button2");
-let changeText = document.querySelector("p");
-
-btn2.addEventListener("click", function () {
-  changeText.innerHTML = "ボタンが押されました";
-  // this.innerHTMLを使用するとHTML要素の中身が変更できる。
-});
+let btn3 = document.getElementById("#Button3");
+changeText = document.querySelector("p");
+btn3.addEventListener("click", function () {
+  changeText.classList.add("red");
+  // this.classList.add("クラス名")でクラス名を追加できるため、CSSに記入されていたら適応される
+})
